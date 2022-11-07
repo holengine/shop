703c8eebadf57aa1ef879ed4bdf6c7181146c0a2
hol3ng <savinyhal@gmail.com>
2022-11-07 19:45:53 +0300

    add cart

--- main.rb
+++ main.rb
@@ -4,36 +4,31 @@ require_relative 'lib/film'
 require_relative 'lib/disk'
 
 require_relative 'lib/product_collection'
+require_relative 'lib/shopping_cart'
 
-products = ProductCollection.from_dir("#{File.dirname(__FILE__)}/data")
+products = ProductCollection.from_dir("#{File.dirname(__FILE__)}/data").to_a
+cart = ShoppingCart.new
 
 user_choice = -1
-choice_products = []
-sum = 0
 
 until user_choice.zero?
   puts 'Что хотите купить:'
-  products.to_a.each_with_index do |product, number|
+  products.each_with_index do |product, number|
     print "#{number + 1}. "
     product.to_s
   end
   puts '0. Выход'
 
-  user_choice = gets.chomp.to_i
+  user_choice = $stdin.gets.to_i
 
-  unless user_choice.positive? && user_choice <= products.to_a.count && products.to_a[user_choice - 1].amount.positive?
-    next
-  end
+  next unless user_choice.positive? && user_choice <= products.count && products[user_choice - 1].amount.positive?
 
   print 'Вы выбрали: '
-  products.to_a[user_choice - 1].amount -= 1
-  products.to_a[user_choice - 1].to_s
-  choice_products << products.to_a[user_choice - 1]
-  sum += products.to_a[user_choice - 1].price
-  puts "Всего товаров на сумму: #{sum}"
+  cart.add_to_cart(products[user_choice - 1])
+  puts "Всего товаров на сумму: #{cart.total_price}"
   puts
 end
 
 puts 'Вы купили:'
-choice_products.each(&:to_s)
-puts "С Вас — #{sum} руб. Спасибо за покупки!"
+cart.display_cart
+puts "С Вас — #{cart.total_price} руб. Спасибо за покупки!"
